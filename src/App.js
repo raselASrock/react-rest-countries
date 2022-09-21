24f4042e4acdfa6b886ceb8ebe6779315ccf622f
Rasel <mdraselm413@gmail.com>
2022-09-21 15:38:40 +0600

    folder style

--- src/App.js
+++ src/App.js
@@ -20,13 +20,26 @@ function LoadCountries(){
     .then( res => res.json())
     .then(data => setCountries(data))
   }, [])
+
   return(
     <div>
       <h1>Visiting Every Country With Me.</h1>
       <h2>Experience With Me. Let's GO!!!!!!!!</h2>
       <h4>Available Countries: {countries.length}</h4>
+      {
+        // countries.map(country => console.log(country))
+        countries.map(country => <Country name ={country.name.common} population = {country.population} officialName = {country.name.official}></Country>)
+      }
     </div>
   )
 }
-
+function Country(props){
+  return(
+    <div>
+      <h2>Country Name: {props.name}</h2>
+      <h2>Official Name: {props.officialName}</h2>
+      <h4>Total Population: {props.population}</h4>
+    </div>
+  )
+}
 export default App;
